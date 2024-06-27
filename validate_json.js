4215dd385fbf424c783f9ae1167902b755c86b32
Ricky Martinez <RMartinezW16@gmail.com>
2024-06-27 12:00:46 -0400

    step 2

--- validate_json.js
+++ validate_json.js
@@ -21,7 +21,7 @@ function validateSimpleJSONObject(jsonObject) {
 
 function validateJSON(inputString) {
     try {
-        const jsonObject = JSON.parse(inputString); //parse string into a javascript obect
+        const jsonObject = JSON.parse(inputString); //parse input into a javascript obect
         if (validateSimpleJSONObject(jsonObject)) { //if valid json obejct and not null print success
             console.log("Valid JSON object.");
             process.exit(0);
