40bba8b44606e4fe4048e89bb092ef344f7fda09
Ricky Martinez <RMartinezW16@gmail.com>
2024-06-27 11:59:59 -0400

    step 2

--- validate_json.js
+++ validate_json.js
@@ -1,9 +1,28 @@
 const fs = require('fs'); //allows you to interact with the file system
 
+
+function isString(value) { //checks if value is a string
+    return typeof value === 'string' || value instanceof String;
+}
+
+function validateSimpleJSONObject(jsonObject) {
+    //check if the jsonObject is not an object, or null or an array
+    if (typeof jsonObject !== 'object' || jsonObject === null || Array.isArray(jsonObject)) {
+        return false;
+    }
+    for (const key in jsonObject) {
+        //check if the type of each key is not a string and each value is not a string
+        if (typeof key !== 'string' || !isString(jsonObject[key])) {
+            return false;
+        }
+    }
+    return true;
+}
+
 function validateJSON(inputString) {
     try {
         const jsonObject = JSON.parse(inputString); //parse string into a javascript obect
-        if (typeof jsonObject === 'object' && jsonObject !== null) { //if valid json obejct and not null print success
+        if (validateSimpleJSONObject(jsonObject)) { //if valid json obejct and not null print success
             console.log("Valid JSON object.");
             process.exit(0);
         } else { //if not valid json object or null print fail
